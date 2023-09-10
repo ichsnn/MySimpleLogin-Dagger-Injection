45ca5fb588f4ff515134d9180b03be8070ffa7ec
ichsnn <ichsan.nulmuhlis@gmail.com>
2023-09-10 23:13:04 +0700

    codelab dagger

---
+++ app/src/main/java/com/dicoding/mysimplelogin/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.dicoding.mysimplelogin
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.dicoding.mysimplelogin.databinding.ActivityMainBinding
+import javax.inject.Inject
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    @Inject
+    lateinit var userRepository: UserRepository
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        (application as MyApplication).appComponent.inject(this)
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (userRepository.isUserLogin()) {
+            moveToHomeActivity()
+        }
+
+        binding.btnLogin.setOnClickListener {
+            saveSession()
+        }
+    }
+
+    private fun saveSession() {
+        userRepository.loginUser(binding.edUsername.text.toString())
+        moveToHomeActivity()
+    }
+
+    private fun moveToHomeActivity() {
+        startActivity(Intent(this, HomeActivity::class.java))
+        finish()
+    }
+}
